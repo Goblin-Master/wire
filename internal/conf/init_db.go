b4658ee64b96a53e07bfdd599fae597180cafa45
Goblin-master <2896151192@qq.com>
2025-03-23 13:33:20 +0800

    feat: 更新数据库初始化配置，添加连接池参数和日志级别

--- internal/conf/init_db.go
+++ internal/conf/init_db.go
@@ -1,19 +1,31 @@
 package conf
 
 import (
+	"wire/internal/config"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"wire/internal/config"
 )
 
 func InitDB() *gorm.DB {
 	db, err := gorm.Open(mysql.Open(config.Conf.DB.DSN()), &gorm.Config{
-		SkipDefaultTransaction: true,
-		Logger:                 logger.Default.LogMode(logger.Silent), // 必须为Silent
+		DisableForeignKeyConstraintWhenMigrating: true, //不生成外键约束
+		Logger:                                   logger.Default.LogMode(logger.Info),
+		PrepareStmt:                              true,
 	})
 	if err != nil {
 		panic(err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	// 设置连接池参数
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
 	return db
 }
